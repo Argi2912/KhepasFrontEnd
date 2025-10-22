f596b2e7914036559befbbad96bf7ab699d02810
Argi2912 <argimirojrodriguezrr@gmail.com>
2025-10-21 22:45:17 -0400

    permission service

---
+++ src/services/permissionsService.js
@@ -0,0 +1,24 @@
+import api from './api'
+
+const resourse = '/permissions/'
+
+export default {
+  getAllPermissions() {
+    return api.get(resourse)
+  },
+  createPermission(permissionData) {
+    return api.post(resourse, permissionData)
+  },
+  createRole(roleData) {
+    return api.post(resourse + 'role', roleData)
+  },
+  rolePermissions(data) {
+    return api.post(resourse + 'role-permissions', data)
+  },
+  userRole(data) {
+    return api.post(resourse + 'user-roles', data)
+  },
+  userPermissions(data) {
+    return api.post(resourse + 'user-permissions', data)
+  },
+}
